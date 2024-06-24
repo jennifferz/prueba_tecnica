3bca209c4f61b1e36f127d904777f1ff64078631
kyc-jenniffer <jenniffer@kyc-systems.com>
2024-06-24 10:17:55 -0600

    cambio de prueba

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include 'navbar.php'; ?>
+<?php include 'conexion.php'; ?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio</title>
+</head>
+<body>
+
+    <header style="text-align: center">  <br>
+        <h2>Bienvenido a Mi Sitio Web</h2>
+
+        <br>
+        <h3>hice un  nuevo cambio / -- 10:05am  24/06/2024</h3>
+
+    </header>
+
+ 
